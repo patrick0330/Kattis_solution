37166b3691da0728fbed8127beaf8857dd659419
patrick0330 <74896573+patrick0330@users.noreply.github.com>
2022-06-28 21:21:22 +0800

    Add files via upload

---
+++ Greetings.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    for(int i = 0;i < s.length();i++){
+        if(s[i] == 'e'){
+            cout << "ee";
+        }
+        else{
+            cout << s[i];
+        }
+    }
+    cout << endl;
+    return 0;
+}
